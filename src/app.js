e4bfeb751a8a26f18f24700642b5f45cd52aafbc
SIILI\mikko.partanen <mikko.partanen@siili.com>
2021-12-03 18:30:53 +0200

    SETUP for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocodeUtils = require('./utils/geocode')
 const weatherforecastUtils = require('./utils/weatherforecast')
 
 const app = express();
+const port = process.env.PORT == 3000;
 
 //define paths for express
 const publicDirectoryPath = path.join(__dirname, '/../public');
@@ -97,6 +98,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 });
